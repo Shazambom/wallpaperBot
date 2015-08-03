af7bd08cb68902b4d166d45b6de3329a864567fe
Shazambom <mrderp14@gmail.com>
2015-08-02 21:34:12 -0400

    Timer re-added
    Kinda need it for now. Thinking about shedualing it a better way so it
    doesn't use so much RAM or leak memory.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
-
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Shazambom on 7/22/2015.
  */
 public class Main {
     public static void main(String[] args) {
-        Organizer organizer = new Organizer("http://boards.4chan.org/w/", "F:\\RippedWallpapers\\");
-        organizer.runRip();
+//        Organizer organizer = new Organizer("http://boards.4chan.org/w/", "F:\\RippedWallpapers\\");
+//        organizer.runRip();
+        Timer timer = new Timer();
+        TimerTask hourlyTask = new TimerTask() {
+            @Override
+            public void run () {
+                Organizer organizer = new Organizer("http://boards.4chan.org/w/", "F:\\RippedWallpapers\\");
+                organizer.runRip();
+            }
+        };
+        timer.schedule(hourlyTask, 0l, 1000*60*60*3);
     }
 }
