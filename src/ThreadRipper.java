793725aa088374641db1c23f703955ec7dc3825b
Shazambom <mrderp14@gmail.com>
2015-08-08 17:24:30 -0400

    Fixed a small bug
    All of these bugs are Mac specific. I may have to rewrite this thing to
    work in a bash environment.

--- src/ThreadRipper.java
+++ src/ThreadRipper.java
@@ -53,8 +53,10 @@ public class ThreadRipper {
             }
             String[] folder = new File(filePath).list();
             HashMap<Integer, String> files = new HashMap<Integer, String>();
-            for (String element: folder) {
-                files.put(element.hashCode(), element);
+            if (folder != null) {
+                for (String element : folder) {
+                    files.put(element.hashCode(), element);
+                }
             }
 
             System.out.print("[");
