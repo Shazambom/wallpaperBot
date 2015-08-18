f044670384910038fa5843d42a5224edd260d960
Yash Punjabi <yashrpunjabi@gmail.com>
2015-08-18 17:35:20 -0400

    Album size limited to 150

--- src/imgurbot.py
+++ src/imgurbot.py
@@ -2,6 +2,8 @@ import glob       #to get filenames
 import pyimgur    #to upload files to imgur
 import praw       #to post links to reddit/r/slashw
 
+# /media/UNTITLED/Wallpapers/
+
 CLIENT_ID = '2e6582b4e4109df'
 CLIENT_SECRET = '9a0e29fb2220d772a81a56a0d3a4f9fee9d8b29b'
 JPG_PATH = '/home/yash/Ian4chanProject/Images/*.jpg'
@@ -23,6 +25,9 @@ SUBREDDIT = 'slashw'
 imgur = pyimgur.Imgur(CLIENT_ID, CLIENT_SECRET)
 image_links = []
 
+def assign_directory_by_time():
+    
+
 def upload_to_imgur():
     
     filenames = []
@@ -42,17 +47,29 @@ def upload_to_imgur():
         thread = filename.split('_', 1)[0]  #returns everything before the first encountered underscore
         if (thread not in already_done):
             album = []
+            album2 = []
             for filename2 in filenames:
                 thread2 = filename2.split('_', 1)[0]
                 if (thread == thread2):
-                    album.append(filename2)
+                    if (album.length > 149):
+                        album2.append(filename2)
+                    else:
+                        album.append(filename2)
             album_images = []
+            album2_images = []
             for image_filename in album:
                 album_images.append(imgur.upload_image(image_filename))
                 print ('uploaded')
+            if (album.length > 149):
+                for image_filename in album2:
+                    album2_images.append(imgur.upload_image(image_filename))
+                    print ('uploaded')
             album_title = thread.rsplit('/', 1)[-1]  #removes everything before last '/'
             image_links.append(imgur.create_album(title=album_title, images=album_images).link)
             print ('album created')
+            if (album.length > 149):
+                image_links.append(imgur.create_album(title=album_title, images=album2_images).link)
+                print ('album2 created')
             already_done.append(thread)
 
 
