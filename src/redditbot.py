18f2b1a2cc3bf117de79ba71e21f7013e9787ce3
Shazambom <Shazambom@users.noreply.github.com>
2015-08-24 14:55:18 -0400

    so that it can be run from outside src

--- src/redditbot.py
+++ src/redditbot.py
@@ -13,7 +13,7 @@ def assign_directory_by_time():
 
 PATH_BASE = '/media/UNTITLED/Wallpapers/' + assign_directory_by_time()
 
-config = open('../../config.txt', 'r')
+config = open('../config.txt', 'r')
 
 CLIENT_ID = config.readline()
 CLIENT_SECRET = config.readline()
